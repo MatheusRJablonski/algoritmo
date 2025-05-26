2b3af948da38115a9eefc02750ceab7236343808
MatheusRJablonski <theus.jablonski@gmail.com>
2025-05-26 14:51:08 -0300

    Update trabalho.cpp

--- trabalho.cpp
+++ trabalho.cpp
@@ -6,15 +6,16 @@ vector<string> times = {
         "Avai",
         "Barra",
         "Brusque",
+        "Caravaggio",
         "Chapecoense",
         "Concordia",
         "Criciuma",
         "Figueirense",
         "Hercilio Luz",
-        "Inter de Lages",
         "Joinville",
         "Marcilio Dias",
-        "Nacao"
+        "Santa Catarina",
+        "Nenhum"
     };
 
 bool idTime(string time, int i = 0){
@@ -31,27 +32,38 @@ bool idTime(string time, int i = 0){
 }
 
 int main(){
-    
+    vector<bool>verificartime = { true , true};
+    int i;
     string time1,time2;
     cout << "Qual seu primeiro time e segundo time:" << endl<< endl;
     
-        for(int i = 0;i < times.size();i++){ // escrever os times na tela
+        for(i = 0;i < times.size();i++){ // escrever os times na tela
         cout << times[i] << endl;
     
         }
         
-    cin >> time1 >> time2; //input dos times
-
-
-    if(idTime(time1)){ //verificar se digitou um time valido
-        cout << "time 1 valido" << endl;
-    }else{
-        cout << "time 1 invalido" << endl;
-    }
-    if(idTime(time2)){ //verificar se digitou um time valido
-        cout << "time 2 valido" << endl;
-    }else{
-        cout << "time 2 invalido" << endl;
+    while(verificartime[0]||verificartime[1]){//enquanto true reescreva valor
+        cin >> time1 >> time2; //input dos times
+        while(time1==time2){
+            cout << "nao insira valores iguais, tente novamente"<< endl;
+            cin >> time2;
+        }
+        if(idTime(time1)){ //verificar se digitou um time valido
+            cout << "time 1 valido" << endl;
+            verificartime[0] = false;
+        }else{
+            cout << "time 1 invalido" << endl;
+        }
+        if(idTime(time2)){ //verificar se digitou um time valido
+            cout << "time 2 valido" << endl;
+            verificartime[1] = false;
+        }else{
+            cout << "time 2 invalido" << endl;
+        }
+        if(verificartime[0]||verificartime[1]){
+            cout << "verifique se escreveu corretamente" <<endl;
+        }
+        
     }
 
 }
