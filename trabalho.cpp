28f8c868301bc5fe50f92a3807b360d6c3c1e7df
MatheusRJablonski <theus.jablonski@gmail.com>
2025-05-26 13:53:03 -0300

    Update trabalho.cpp

--- trabalho.cpp
+++ trabalho.cpp
@@ -18,11 +18,11 @@ vector<string> times = {
     };
 
 bool idTime(string time, int i = 0){
-
+//verificar se o time esta no campionato ou nao
     if(time == times[i]){
         return true;
-    }if(i==(times.size()-1)){
-        return false;
+    }if(i==(times.size()-1)){//caso tenham mais times  o .size ajuda a ter controle da quantidade
+        return false; 
     }else{
         return idTime(time,++i);
     }
@@ -34,18 +34,21 @@ int main(){
     
     string time1,time2;
     cout << "Qual seu primeiro time e segundo time:" << endl<< endl;
-    for(int i = 0;i < times.size();i++){
+    
+        for(int i = 0;i < times.size();i++){ // escrever os times na tela
         cout << times[i] << endl;
-    }
-    cin >> time1 >> time2;
+    
+        }
+        
+    cin >> time1 >> time2; //input dos times
 
 
-    if(idTime(time1)){
+    if(idTime(time1)){ //verificar se digitou um time valido
         cout << "time 1 valido" << endl;
     }else{
         cout << "time 1 invalido" << endl;
     }
-    if(idTime(time2)){
+    if(idTime(time2)){ //verificar se digitou um time valido
         cout << "time 2 valido" << endl;
     }else{
         cout << "time 2 invalido" << endl;
