cc927aa6baa16cce3287d0481cd4cdbd12a3a09b
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2025-06-23 21:14:16 -0300

    finalizado

---
+++ funcoes.hpp
@@ -0,0 +1,13 @@
+#ifndef FUNCOES_HPP
+#define FUNCOES_HPP
+
+#include <string>
+using namespace std;
+
+// Funções que precisam estar aqui:
+string JogosGrupos(int a, int b);
+string JogosMata(int a, int b);
+int valorTime1(string time1);
+int valorTime2(string time2);
+
+#endif
